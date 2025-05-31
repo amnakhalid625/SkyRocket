b2051d51d02dacdc6420546e2c3e8207b26b497f
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-05-31 15:38:37 +0500

    pricing

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -4,7 +4,7 @@ import tickImg from '../assets/images/tick.svg';
 import whyUsImg from '../assets/images/whyChoose.webp';
 
 
-const Mission = () => {
+const WhyUs = () => {
   return (
     <section className="py-12 md:py-[80px] bg-white">
       <div className="px-4 sm:px-6 md:px-8 lg:px-12 xl:px-20">
@@ -24,7 +24,7 @@ const Mission = () => {
               </h2>
 
           
-           {/* Updated List with ticks */} <div className="flex flex-col gap-3 max-w-lg"> {[ { title: "Transparent Pricing", desc: "No hidden fees; clear and upfront costs.", }, { title: "Tailored Solutions", desc: "We understand the Detroit market and customize our services to meet local business needs.", }, { title: "Expert Team", desc: "Work with a team of experienced professionals dedicated to your success.", }, { title: "Proven Results", desc: "Our strategies are designed to drive traffic, increase engagement, and boost conversions.", }, ].map((item, index) => ( <div className="flex items-start gap-3" key={index}> <img src="https://cdn.prod.website-files.com/6807e00032c9ff6c11834a8f/6807e00132c9ff6c11834bd9_circle-check-filled%20(1).svg" alt="tick icon" className="w-6 h-6 mt-1 flex-shrink-0" /> <div> <div className="text-textColor font-semibold text-[16px]"> {item.title} </div> <div className="text-textColor text-sm mt-1"> {item.desc} </div> </div> </div> ))} </div>
+           {/* Updated List with ticks */} <div className="flex flex-col gap-3 max-w-lg"> {[ { title: "Transparent Pricing", desc: "No hidden fees; clear and upfront costs.", }, { title: "Tailored Solutions", desc: "We understand the Detroit market and customize our services to meet local business needs.", }, { title: "Expert Team", desc: "Work with a team of experienced professionals dedicated to your success.", }, { title: "Proven Results", desc: "Our strategies are designed to drive traffic, increase engagement, and boost conversions.", }, ].map((item, index) => ( <div className="flex items-start gap-3" key={index}> <img src={tickImg} alt="tick icon" className="w-6 h-6 mt-1 flex-shrink-0" /> <div> <div className="text-textColor font-semibold text-[16px]"> {item.title} </div> <div className="text-textColor text-sm mt-1"> {item.desc} </div> </div> </div> ))} </div>
 
               {/* Button */}
               <div>
@@ -61,4 +61,4 @@ const Mission = () => {
   );
 };
 
-export default Mission;
+export default WhyUs;
